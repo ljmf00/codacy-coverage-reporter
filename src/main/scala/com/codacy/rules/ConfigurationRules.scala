4eac99d3cacb634bae1da7be0c1e3048e70c973a
IGFCoimbra <36641997+IGFCoimbra@users.noreply.github.com>
2019-03-29 17:50:13 +0000

    Add support to scrutinizer-ci
    https://scrutinizer-ci.com/docs/build/environment-variables

--- src/main/scala/com/codacy/rules/ConfigurationRules.scala
+++ src/main/scala/com/codacy/rules/ConfigurationRules.scala
@@ -109,7 +109,8 @@ class ConfigurationRules(cmdConfig: CommandConfiguration) extends StrictLogging 
       getNonEmptyEnv("WERCKER_GIT_COMMIT") orElse
       getNonEmptyEnv("CODEBUILD_RESOLVED_SOURCE_VERSION") orElse
       getNonEmptyEnv("CI_COMMIT_SHA") orElse
-      getNonEmptyEnv("HEROKU_TEST_RUN_COMMIT_VERSION")
+      getNonEmptyEnv("HEROKU_TEST_RUN_COMMIT_VERSION") orElse
+      getNonEmptyEnv("SCRUTINIZER_SHA1")
         .filter(_.trim.nonEmpty)
   }
 
